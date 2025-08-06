df4c1bcf70414e0119a955355cf8da3aac2ab9f2
samuel maveke <mavekepeter2022@gmail.com>
2025-08-06 11:32:23 +0300

    theme provider and dark and light togglemode implementation

---
+++ app/(tabs)/settings.tsx
@@ -0,0 +1,13 @@
+import {  Text, View } from 'react-native'
+import React from 'react'
+
+const SettingsScreen = () => {
+  return (
+    <View>
+      <Text>settings</Text>
+    </View>
+  )
+}
+
+export default SettingsScreen
+
